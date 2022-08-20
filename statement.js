43343bfcc0aa0a23c12c31463e5ba4e283c36b4e
Wenlin Huang <jackhwl@hotmail.com>
2022-08-19 20:49:29 -0400

    remove volumeCredits temp variable

--- statement.js
+++ statement.js
@@ -1,17 +1,14 @@
 function statement (invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
         totalAmount += amountFor(perf);
     }
     result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
     function usd(aNumber) {
@@ -23,9 +20,17 @@ function statement (invoice, plays) {
                 }).format(aNumber/100);
     }
 
+    function totalVolumeCredits(){
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }    
+        return volumeCredits;
+    }
+
     function volumeCreditsFor(aPerformance) {
         let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
+        result = Math.max(aPerformance.audience - 30, 0);
         if ("comedy" === playFor(aPerformance).type)
             result += Math.floor(aPerformance.audience / 5);
         return result;
