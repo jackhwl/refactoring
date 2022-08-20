543e61069354ba4640cac5e8e5f09f318a8f5163
Wenlin Huang <jackhwl@hotmail.com>
2022-08-20 08:31:22 -0400

    Extract totalAmount temp variable

--- statement.js
+++ statement.js
@@ -1,15 +1,19 @@
 function statement (invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
-
     for (let perf of invoice.performances) {
-        // print line for this order
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
-        totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }    
+        return result;
+    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
@@ -21,11 +25,11 @@ function statement (invoice, plays) {
     }
 
     function totalVolumeCredits(){
-        let volumeCredits = 0;
+        let result = 0;
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
+            result += volumeCreditsFor(perf);
         }    
-        return volumeCredits;
+        return result;
     }
 
     function volumeCreditsFor(aPerformance) {
