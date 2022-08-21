030e1d8eb54c8a0cea65509fa6f93448fe2703ca
Wenlin Huang <jackhwl@hotmail.com>
2022-08-21 16:27:36 -0400

    immutable performances

--- statement.js
+++ statement.js
@@ -1,13 +1,18 @@
 function statement (invoice, plays) {
     const statementData = {
         customer: invoice.customer,
-        performances: invoice.performances,
+        performances: invoice.performances.map(enrichPerformance),
         plays: plays
     }
-    return renderPlainText(statementData, invoice, plays);
+    return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        return result
+    }
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
@@ -19,7 +24,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }    
         return result;
@@ -36,7 +41,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalVolumeCredits(){
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }    
         return result;
