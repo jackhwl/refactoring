b51f53ba03afedfe9810e8393dc906141ba466ea
Wenlin Huang <jackhwl@hotmail.com>
2022-08-22 19:32:50 -0400

    add htmlStatement and renderHtml methods

--- statement.js
+++ statement.js
@@ -14,6 +14,24 @@ function renderPlainText(data) {
     return result;
 }
 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += `<table>\n`;
+    result += `<tr><th>play</th><th>seats</th><th>cost</th></tr>`;
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += ` <td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += `</table>`;
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits} </em> credits</p>\n`;
+    return result;
+}
+
 function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
             { 
@@ -30,7 +48,8 @@ const invoices = require('./invoices.json');
 const plays = require('./plays.json');
 //console.log(Intl.NumberFormat.supportedLocalesOf('en'))
 invoices.forEach(invoice => 
-    console.log(statement(invoice, plays))
+    console.log(htmlStatement(invoice, plays))
+    
 )
 
 
