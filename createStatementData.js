aeb5995b24466df26e74c79c1a408e5dca89eebd
Wenlin Huang <jackhwl@hotmail.com>
2022-08-24 22:39:16 -0400

    split class to calculator js file

--- createStatementData.js
+++ createStatementData.js
@@ -1,3 +1,4 @@
+import { TragedyCalculator, ComedyCalculator }  from './calculator.js'
 
 function createPerformanceCalculator(aPerformance, aPlay) {
     switch (aPlay.type) {
@@ -42,42 +43,4 @@ export default function createStatementData(invoice, plays) {
             .reduce((total, p) => total + p.volumeCredits, 0)
     }
     
-}
-
-class PerformanceCalculator {
-    constructor(aPerformance, aPlay) {
-        this.performance = aPerformance;
-        this.play = aPlay;
-    }
-
-    get amount() {
-        throw new Error('subclass responsibility')
-    }
-
-    get volumeCredits() {
-        return Math.max(this.performance.audience - 30, 0);
-    }
-}
-class TragedyCalculator extends PerformanceCalculator {
-    get amount() {
-        let result = 40000
-        if (this.performance.audience > 30) {
-            result += 1000 * (this.performance.audience - 30)
-        }
-        return result
-    }
-}
-class ComedyCalculator extends PerformanceCalculator {
-    get amount() {
-        let result = 30000;
-        if (this.performance.audience > 20) {
-            result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        return result
-    }
-
-    get volumeCredits() {
-        return super.volumeCredits + Math.floor(this.performance.audience / 5);
-    }
 }
