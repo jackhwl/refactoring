7d06a96c00c7b334ca673de9fc3439616c45054b
Wenlin Huang <jackhwl@hotmail.com>
2022-09-03 09:51:03 -0400

    two more tests

--- ch3/test/test.js
+++ ch3/test/test.js
@@ -1,13 +1,24 @@
+import { assert, expect } from "chai";
+
 import { sampleProvinceData } from '../src/run.js'
 import { Province } from '../src/province.js'
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-var assert = require('assert');
 
 
 describe('province', function () {
+    let asia;
+    beforeEach(function () {
+        asia = new Province(sampleProvinceData())
+    })
     it('shortfall', function () {
-        const asia = new Province(sampleProvinceData())
         assert.equal(asia.shortfall, 5)
+        expect(asia.shortfall).equal(5);
+    })
+    it('profit', function () {
+        expect(asia.profit).equal(230);
+    })
+    it('change production', function () {
+        asia.producers[0].production = 20
+        expect(asia.shortfall).equal(-6);
+        expect(asia.profit).equal(292);
     })
 })
