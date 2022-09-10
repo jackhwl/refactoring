285019a00a0da667dabe7280cb648610d36d4bf8
Wenlin Huang <whuang@viglobal.com>
2022-09-10 10:02:37 -0400

    Reassigning a Local Variable

--- ch6/printOwing.js
+++ ch6/printOwing.js
@@ -1,28 +1,38 @@
 function printOwing(invoice) {
-    let outstanding = 0
+    
 
     printBanner()
 
-    // calculate outstanding
-    for (const o of invoice.orders) {
-        outstanding += o.amount
-    }
+    const outstanding = calculateOutstanding(invoice)
 
-    // record due date
-    const today = Clock.today
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate()) 
+    recordDueDate(invoice) 
     
-    printDetails()
+    printDetails(invoice, outstanding)
 
-    function printDetails() {
-        console.log(`name: ${invoice.customer}`)
-        console.log(`amount: ${outstanding}`)
-        console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
-    }
+
 
     function printBanner() {
         console.log("***********************")
         console.log("**** Customer Owes ****")
         console.log("***********************")
     }
+}
+
+function calculateOutstanding(invoice) {
+    let result = 0
+    for (const o of invoice.orders) {
+        result += o.amount
+    }
+    return result
+}
+
+function printDetails(invoice, outstanding) {
+    console.log(`name: ${invoice.customer}`)
+    console.log(`amount: ${outstanding}`)
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate())
 }
